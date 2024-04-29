357625d54532679079498ec6871ed412f9ca5e74
FranzyGD <166528069+FranzyGD@users.noreply.github.com>
2024-04-29 14:01:39 -0300

    Update main.js

--- boletines/javascript/main.js
+++ boletines/javascript/main.js
@@ -10,7 +10,7 @@ cerrar.forEach(function (tonto) {
     content.classList.add("animate__animated");
 
     setTimeout(function () {
-      location.href = "/boletines/index.html";
+      location.href = "index.html";
     }, 900);
 
     return false;

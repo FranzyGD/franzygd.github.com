b49383f2e3ce343fda62fdb4084356304cb5bf04
FranzyGD <166528069+FranzyGD@users.noreply.github.com>
2024-04-29 14:20:49 -0300

    Update main.js

--- boletines/javascript/main.js
+++ boletines/javascript/main.js
@@ -10,7 +10,7 @@ cerrar.forEach(function (tonto) {
     content.classList.add("animate__animated");
 
     setTimeout(function () {
-      history.back;
+      history.back();
     }, 900);
 
     return false;
